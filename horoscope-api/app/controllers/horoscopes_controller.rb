18babb449e8ae4dd8c7f08f38286977c3f071cc1
Lauren McCoy <lm_lutec@outlook.com>
2020-05-01 20:39:47 -0400

    working on how to call Scraper class

--- horoscope-api/app/controllers/horoscopes_controller.rb
+++ horoscope-api/app/controllers/horoscopes_controller.rb
@@ -1,13 +1,8 @@
-require 'nokogiri'
-require 'open-uri'
-
-
 class HoroscopesController < ApplicationController
-    @@arr = []
 
     def index
        horoscopes = Horoscope.all 
-       daily_page
+       Scraper.new.daily_page
        render json: horoscopes, only:[:name, :start_date, :end_date, :symbol, :house, :planet, :element, :quality, :polarity], include: [:dailies]
     end 
     
@@ -20,32 +15,7 @@ class HoroscopesController < ApplicationController
         render json: horoscope
     end 
 
-    def daily_page
-
-        site = open("https://astrostyle.com/horoscopes/daily/")
-        doc = Nokogiri::HTML(site)
-
-        doc.css('.my-horoscope-table-wrap').css('a').each do |i|
-            @@arr << i.attr('href')
-        end 
-        daily_readings
-    end 
-
-    def daily_readings
-        @@arr.uniq!.each do |d|
-            site = open(d)
-            doc = Nokogiri::HTML(site)
-            reading = doc.css('.horoscope-content').css('p').text
-            date = doc.css('.horoscope-content').css('h2').text
-            sign = d.split("/").last.capitalize
-            find_horoscope = Horoscope.find_by(name: sign) 
-            find_horoscope.dailies.each do |i|
-                if i.date != date
-                    Daily.create(horoscope_id: find_horoscope.id, date: date, text: reading)
-                end 
-            end 
-        end 
-    end 
+   
 end
 
 
---
+++ horoscope-api/app/models/scraper.rb
@@ -0,0 +1,35 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper < ApplicationController
+
+    @@arr = []
+
+    def daily_page
+        site = open("https://astrostyle.com/horoscopes/daily/")
+        doc = Nokogiri::HTML(site)
+
+        doc.css('.my-horoscope-table-wrap').css('a').each do |i|
+            @@arr << i.attr('href')
+        end 
+        daily_readings
+    end 
+
+    def daily_readings
+        @@arr.uniq!.each do |d|
+            site = open(d)
+            doc = Nokogiri::HTML(site)
+            reading = doc.css('.horoscope-content').css('p').text
+            
+            date = doc.css('.horoscope-content').css('h2').children[0].text.strip
+            sign = d.split("/").last.capitalize
+            find_horoscope = Horoscope.find_by(name: sign) 
+            
+            find_horoscope.dailies.each do |i|
+                if i.date != date
+                    Daily.create(horoscope_id: find_horoscope.id, date: new_date, text: reading)
+                end 
+            end 
+        end 
+    end 
+end 
