34dc56a59ea31c1b22ff8a48da768fb139ae8958
Lauren McCoy <lm_lutec@outlook.com>
2020-05-03 11:00:03 -0400

    refactoring js page to add a section to each horoscope and moving towards event listeners

--- horoscope-frontend/index.js
+++ horoscope-frontend/index.js
@@ -8,14 +8,42 @@ fetch(HOROSCOPES_URL)
         return response.json();
     })
     .then(function(json){
-        console.log(json)
-        // json.forEach(element => {
-        //     let sign = document.createElement("LI") 
-        //     sign.innerHTML = element.name    
-        //     document.body.appendChild(sign)
-        // });
+        //console.log(json)
+        json.data.forEach(element => {
+            s = document.getElementsByClassName('sign')
+           
+            for (const ele of s){
+                console.log(ele)
+                let sec = document.createElement("section")
+
+                // if (element.attributes.name.toLowerCase() == ele.alt){
+                //     p = document.createElement('p')
+                //     ele.appendChild(p)
+                //     p.innerText = `${element.attributes.name} , ${element.attributes.house}`
+                // } **WORKING CODE
+               
+                // let splitSigns = ele.alt.split("")
+                // let changeCase = splitSigns[0].toUpperCase()
+                // splitSigns.shift()
+                // splitSigns.unshift(changeCase)
+                // let rejoinSigns = splitSigns.join(" ")
+                
+                
+            }
+        
+            // if (element.attributes.name == element.alt.toUpperCase()){
+            //     console.log('true')
+            // }
+            // else {
+            //     console.log('false')
+            // }
+            // let sign = document.createElement("LI") 
+            // sign.innerHTML = element.name    
+            // document.body.appendChild(sign)
+        });
         
       })
+
 
 
 
@@ -28,19 +56,19 @@ fetch(HOROSCOPES_URL)
         
 //    });
         
-s = document.getElementsByClassName('sign')
+// s = document.getElementsByClassName('sign')
 
 
-for (const element of s) {
-    console.log(element.alt)
-    element.addEventListener("click", function(){
-        let d = document.createElement("dialog")
-        d.open = true
-        let p = document.createElement("p")
-        document.body.appendChild(d)  
-        d.appendChild(p)
-        p.innerText = "true"
-    })
-  }
+// for (const element of s) {
+//     console.log(element.alt)
+//     element.addEventListener("click", function(){
+//         let d = document.createElement("dialog")
+//         d.open = true
+//         let p = document.createElement("p")
+//         document.body.appendChild(d)  
+//         d.appendChild(p)
+//         p.innerText = "true"
+//     })
+//   }
 
  
