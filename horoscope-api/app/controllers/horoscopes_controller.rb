307f12436f1b07b9a0db67b9de6ff8bc073e4654
Lauren McCoy <lm_lutec@outlook.com>
2020-05-05 18:41:38 -0400

    scraper works inside of horoscope

--- horoscope-api/app/controllers/horoscopes_controller.rb
+++ horoscope-api/app/controllers/horoscopes_controller.rb
@@ -1,7 +1,14 @@
+require 'nokogiri'
+require 'open-uri'
+
+
 class HoroscopesController < ApplicationController
 
+    @@arr = []
+
     def index
-       Scraper.new.daily_page
+        daily_page
+       
        horoscopes = Horoscope.all 
 
        options = {
@@ -19,6 +26,30 @@ class HoroscopesController < ApplicationController
         render json: HoroscopeSerializer.new(horoscope)
     end 
 
+
+    def daily_page
+        site = open("https://astrostyle.com/horoscopes/daily/")
+        doc = Nokogiri::HTML(site)
+
+        doc.css('.my-horoscope-table-wrap').css('a').each do |i|
+            @@arr << i.attr('href')
+        end 
+        daily_readings
+    end 
+
+    def daily_readings
+        @@arr.uniq!.each do |d|
+            site = open(d)
+            doc = Nokogiri::HTML(site)
+            reading = doc.css('.horoscope-content').css('p').text
+            
+            date = doc.css('.horoscope-content').css('h2').children[0].text.strip
+            sign = d.split("/").last.capitalize
+            find_horoscope = Horoscope.find_by(name: sign) 
+            
+            find_horoscope.dailies << Daily.create(horoscope_id: find_horoscope.id, date: date, text: reading)
+        end 
+    end 
 end
 
 
