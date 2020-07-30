1426b24a7b85fd5be379411b10f120c11deec40c
Lauren McCoy <lm_lutec@outlook.com>
2020-07-30 19:47:15 -0400

    changed links on html page

--- horoscope-api/app/controllers/dailies_controller.rb
+++ horoscope-api/app/controllers/dailies_controller.rb
@@ -7,9 +7,8 @@ class DailiesController < ApplicationController
     end 
 
     def create
-        daily = Daily.new(date: params[:dateReceived], source: params[:sourceReceived], text: params[:readingReceived])
+        daily = Daily.create(date: params[:dateReceived], source: params[:sourceReceived], text: params[:readingReceived])
         daily.horoscope_id = params[:horoscope_id]
-        daily.save 
     end 
      
 end
